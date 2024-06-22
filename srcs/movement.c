782b528fc3913897e805a140fe812e5c4bb4ee1b
Lopie13 <miguelmatalvesbarradas@gmail.com>
2024-06-22 16:46:05 +0100

    hmmm

---
+++ srcs/movement.c
@@ -0,0 +1,78 @@
+#include "so_long.h"
+
+/* void	collected(t_vars *v)
+{
+	v->collect--;
+	v->map[(v->y_p / 32)][(v->x_p / 32)] = '0';
+	if (v->collect == 0)
+		mlx_put_image_to_window(v->mlx, v->win, v->assets->gwd->img,
+			v->e_x, v->e_y);
+} */
+
+void	move_left(t_vars *v)
+{
+	mlx_put_image_to_window(
+		v->mlx, v->win, v->assets->ground->img, v->x_p, v->y_p);
+	if (v->map[(v->y_p / 32)][(v->x_p / 32) - 1] == 'E')
+		exit_verifier(v, 0, 0);
+	else if (v->map[(v->y_p / 32)][(v->x_p / 32) - 1] != '1')
+	{
+		v->x_p -= 32;
+		put_text(v);
+	}
+	if (v->map[(v->y_p / 32)][(v->x_p / 32)] == 'C')
+		collected(v);
+	v->left = 1;
+/* 	mlx_put_image_to_window(v->mlx, v->win,
+		v->assets->gwc2->img,
+		v->x_p, v->y_p); */
+}
+
+void	move_right(t_vars *v)
+{
+	mlx_put_image_to_window(v->mlx, v->win,
+		v->assets->ground->img, v->x_p, v->y_p);
+	if (v->map[(v->y_p / 32)][(v->x_p / 32) + 1] == 'E')
+		exit_verifier(v, 0, 1);
+	else if (v->map[(v->y_p / 32)][(v->x_p / 32) + 1] != '1')
+	{
+		v->x_p += 32;
+		put_text(v);
+	}
+	if (v->map[(v->y_p / 32)][(v->x_p / 32)] == 'C')
+		collected(v);
+	v->left = 0;
+	mlx_put_image_to_window(v->mlx, v->win,
+		v->assets->gwc->img,
+		v->x_p, v->y_p);
+}
+
+void	move_up(t_vars *v)
+{
+	mlx_put_image_to_window(v->mlx, v->win,
+		v->assets->ground->img, v->x_p, v->y_p);
+	if ((v)->map[((v)->y_p / 32) - 1][((v)->x_p / 32)] == 'E')
+		exit_verifier(v, 1, 0);
+	else if ((v)->map[((v)->y_p / 32) - 1][((v)->x_p / 32)] != '1')
+	{
+		(v)->y_p -= 32;
+		put_text(v);
+	}
+	move_idk(v);
+}
+
+void	move_down(t_vars *v)
+{
+	mlx_put_image_to_window(v->mlx, v->win,
+		v->assets->ground->img, v->x_p, v->y_p);
+	if (v->map[(v->y_p / 32) + 1][(v->x_p / 32)] == 'E')
+		exit_verifier(v, 1, 1);
+	else if (v->map[(v->y_p / 32) + 1][(v->x_p / 32)] != '1')
+	{
+		v->y_p += 32;
+		put_text(v);
+	}
+	move_idk(v);
+}
+
+//need to "create" exit_verifier and put_text
